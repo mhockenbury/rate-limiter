bc0c3321cd3c118d01fe4cf7014fea964133ff39
Matthew Hockenbury <mhockenbury@gmail.com>
2024-08-27 17:32:10 -0400

    basic setup for express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function helloworld(message: string) {
-   console.log(`A message to you: ${message}`); 
-}
+import express, { Application } from "express";
+import Server from "./Server";
+
+const app = express();
+const server = new Server(app);
+const PORT = 8080;
+
+app.listen(PORT, "localhost", function() {
+   console.log("Server starting on", `localhost:${PORT}`);
+}).on("error", function(error) {
+   console.log("Application Error:", error)
+})
